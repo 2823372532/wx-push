a43381d57d91a1cdab5a29616edb09d921a3ec06
2823372532 <96641369+2823372532@users.noreply.github.com>
2024-09-19 16:22:28 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wxfe1096a52c8cb9fc",
+  appsecret: "08e05a37536347fb60d448f53de18aa8",
+  template_id: "6pn0FSWEzuHqJh7i1Mgw63LSaGnYjgnmIPjyD4zrnow",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "湖南",
+  first_date: "2024-08-29",
 };
